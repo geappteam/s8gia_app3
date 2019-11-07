83618557473a4e54c1b95746993e7c0a3fb89cff
Anthony Parris <para2709@usherbrooke.ca>
2019-11-07 16:32:10 -0500

    started bayes

---
+++ app2.m
@@ -0,0 +1,9 @@
+%%NOTE: Recommend to understand priori P(Ci)
+%% Bayes parameters
+false_neg_cost = 3;
+false_pos_cost = 1;
+%% Bayes - A : Classification Algorithm (wih hypothesis of Gauss laws only)
+
+%% Bayes - B : Classification Algorithm (without Gauss laws hypothesis, without mathematical expression of frontiers, with Bayes Risk)
+
+%% Bayes - C : Classification Algorithm (with mathematical expression of frontiers, with hypothesis of Gauss Density Probability)
