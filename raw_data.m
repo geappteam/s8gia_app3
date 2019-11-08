c27e7a1a6f3c33b7aa6fff07371cea11dab230ec
Édouard Denommée <dene2303@usherbrooke.ca>
2019-11-07 19:33:01 -0500

    Basic frontier

--- raw_data.m
+++ raw_data.m
@@ -11,40 +11,48 @@ load(data_NP300_path);
 %% Histograms - P300 And NP300
 % E1
 figure()
-nbins = 30;
-histogram(ref_P300(:,1),nbins)
 hold on
-histogram(ref_NP300(:,1),nbins)
-legend('P300','NP300')
+nbins = 30;
+h = histfit(ref_P300(:,1),nbins);
+h(1).FaceAlpha = 0.5;
+h = histfit(ref_NP300(:,1),nbins);
+h(1).FaceAlpha = 0.5;
+legend('P300','' ,'NP300', '')
 title('E1 Parameter Histogram')
 hold off
 
 % E2
 figure()
 nbins = 30;
-histogram(ref_P300(:,2),nbins)
+h = histfit(ref_P300(:,2),nbins);
+h(1).FaceAlpha = 0.5;
 hold on
-histogram(ref_NP300(:,2),nbins)
-legend('P300','NP300')
+h = histfit(ref_NP300(:,2),nbins);
+h(1).FaceAlpha = 0.5;
+legend('P300','' ,'NP300', '')
 title('E2 Parameter Histogram')
 hold off
 
 % E3
 figure()
 nbins = 30;
-histogram(ref_P300(:,3),nbins)
+h = histfit(ref_P300(:,3),nbins);
+h(1).FaceAlpha = 0.5;
 hold on
-histogram(ref_NP300(:,3),nbins)
-legend('P300','NP300')
+h = histfit(ref_NP300(:,3),nbins);
+h(1).FaceAlpha = 0.5;
+legend('P300','' ,'NP300', '')
 title('E3 Parameter Histogram')
 hold off
 
 % E4
 figure()
 nbins = 30;
-histogram(ref_P300(:,4),nbins)
+h = histfit(ref_P300(:,4),nbins);
+h(1).FaceAlpha = 0.5;
 hold on
-histogram(ref_NP300(:,4),nbins)
-legend('P300','NP300')
+h = histfit(ref_NP300(:,4),nbins);
+h(1).FaceAlpha = 0.5;
+legend('P300','' ,'NP300', '')
 title('E4 Parameter Histogram')
 hold off
